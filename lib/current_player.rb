334c00b9d385e89679859aa8c41d4e8a844d8f7c
Vince W <vincewallgren@me.com>
2020-11-08 06:03:19 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,13 +10,13 @@ def turn_count(board)
 end
 
 def turn_count(board)
-  i = 0	
-  count = 0	
-  while i <= 9	
-    if board[i] == "X" || board[i] == "O"	
-      count += 1	
-    end	
+  i = 0
+  count = 0
+  while i <= 9
+    if board[i] == "X" || board[i] == "O"
+      count += 1
+    end
     i += 1	
-  end	
+  end
   count
 end
