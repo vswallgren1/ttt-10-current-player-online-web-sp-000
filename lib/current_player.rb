c88ad9b11e7f559f03833bf4f78a74c4201d8d4a
Vince W <vincewallgren@me.com>
2020-11-08 06:05:41 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,22 +1,11 @@
 def turn_count(board)
-  board.count{|token| token == "X" || token == "O"}
-  turns = 0
-  board.each do |token|
-    if token == "X" || token == "O"
-      turns += 1
-    end
-  end
-  turns
-end
-
-def turn_count(board)
   i = 0
   count = 0
   while i <= 9
     if board[i] == "X" || board[i] == "O"
       count += 1
     end
-    i += 1	
+    i += 1
   end
   count
 end
